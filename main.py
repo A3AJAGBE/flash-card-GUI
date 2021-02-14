505638e3820104c91a2f8ea15e9a375ccddd2c24
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-14 09:37:39 +0000

    Add card text

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ interface.config(padx=50, pady=50, bg="#007bff")
 canvas = Canvas(width=340, height=210, bg="#007bff", highlightthickness=0)
 bg_image = PhotoImage(file="front.png")
 canvas.create_image(170, 105, image=bg_image)
-canvas.grid(column=0, row=0)
+language = canvas.create_text(170, 55, text="Spanish", font=("Arial", 30, "italic"))
+word = canvas.create_text(170, 140, text="Word", font=("Arial", 50, "bold"))
+canvas.grid(column=0, columnspan=2, row=0)
+
 
 # Keeps the interface open until exited
 interface.mainloop()
