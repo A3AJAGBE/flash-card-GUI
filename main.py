914ee6b478b8fdedec31b458cc098a4b9ac77826
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-14 09:08:17 +0000

    Add front card image

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from tkinter import *
 # Setup the User interface
 interface = Tk()
 interface.title("Study the Spanish Language")
+interface.config(padx=50, pady=50, bg="#007bff")
+
+# Add front card image using canvas widget
+canvas = Canvas(width=340, height=210, bg="#007bff", highlightthickness=0)
+bg_image = PhotoImage(file="front.png")
+canvas.create_image(170, 105, image=bg_image)
+canvas.grid(column=0, row=0)
 
 # Keeps the interface open until exited
 interface.mainloop()
