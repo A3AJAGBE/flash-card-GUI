51f8f2e5f9a79f4904d6eb0d9b48ff01efbbcf9a
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-14 10:31:20 +0000

    Change the spanish word

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from tkinter import *
 import pandas as pd
+import random
 
 # Get the data
 data = pd.read_csv("Spanish_words.csv")
 language_data = data.to_dict(orient="records")
-print(language_data)
+
+
+def next_card():
+    """This function displays random words to the user"""
+    current = random.choice(language_data)
+    canvas.itemconfig(language, text="Spanish")
+    canvas.itemconfig(word, text=current["Spanish"])
 
 
 BACKGROUND_COLOR = "#007bff"
@@ -19,16 +26,16 @@ canvas = Canvas(width=340, height=210)
 bg_image = PhotoImage(file="flash_cards/front.png")
 canvas.create_image(170, 105, image=bg_image)
 settings = canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-language = canvas.create_text(170, 55, text="Spanish", font=("Arial", 30, "italic"))
+language = canvas.create_text(170, 55, text="Title", font=("Arial", 30, "italic"))
 word = canvas.create_text(170, 140, text="Word", font=("Arial", 50, "bold"))
 canvas.grid(column=0, columnspan=2, row=0)
 
 cross = PhotoImage(file="verify/unknown.png")
-unknown_button = Button(image=cross, highlightthickness=0)
+unknown_button = Button(image=cross, highlightthickness=0, command=next_card)
 unknown_button.grid(column=0, row=1)
 
 check = PhotoImage(file="verify/known.png")
-known_button = Button(image=check, highlightthickness=0)
+known_button = Button(image=check, highlightthickness=0, command=next_card)
 known_button.grid(column=1, row=1)
 
 # Keeps the interface open until exited
